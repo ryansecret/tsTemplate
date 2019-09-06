9fbd46a8dab243e9a8ff0e5d61663fcc32f6eece
chenjingnan <chenjingnan@jd.com>
2019-09-06 20:46:06 +0800

    feat(ut): ut

--- build/ShapeKind.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Circle = Symbol("circle");
-exports.Square = Symbol("square");
-//# sourceMappingURL=ShapeKind.js.map
--- test/tstest.test.ts
+++ test/tstest.test.ts
@@ -1,14 +1,14 @@
 import * as ShapeKind from '../src/ShapeKind'
 import {area,Square}  from '../src/ShapeFun'
-
+import assert = require('assert');
 describe("jest test",function () {
     it('func',function () {
         let shape: Square={
             sideLength:5,
             kind:ShapeKind.Square
         }
-        expect(area(shape)).toBeGreaterThan(0)
-
+        //expect(area(shape)).toBeGreaterThan(0)
+        assert(area(shape)>0)
     })
 })
 
