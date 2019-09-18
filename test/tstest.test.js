01aa50c7dfb5cb23565dba12fdd981d9d96ae2e2
ryansecret <shaolinshi111@163.com>
2019-09-18 17:35:48 +0800

    feat(power-assert): test framework 改为mocha

--- test/tstest.test.js
+++ test/tstest.test.js
@@ -1,13 +1,21 @@
 'use strict';
 
-const assert = require('assert');
+const assert = require('assert')
 const Tool = require('../src');
 describe('test/index.js', () => {
   const tool = new Tool();
 
 
   it('should hi', () => {
-    assert(tool.hello() === 'hi');
+    assert(tool.hello() === 'hi1');
   });
 
 });
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1, 2, 3].indexOf(3), -1);
+    });
+  });
+});
+
